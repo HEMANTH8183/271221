333c67155011ec97f8f6e796d9aca7b2e515d1ce
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-27 22:40:27 +0530

    series of factorials

---
+++ Factorial_series.java
@@ -0,0 +1,9 @@
+class Factorial_series{  
+ public static void main(String args[]){  
+  int fact=1;   
+  for(int index =1;index<=10;index++){    
+      fact=fact*index;    
+  }    
+  System.out.println(fact);    
+ }  
+}
